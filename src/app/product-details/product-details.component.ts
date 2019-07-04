92c24ff5d5c62abe50959513e8d0ed1e94e8eef4
codegymdanang <codegymdanang@gmail.com>
2019-07-04 16:49:03 +0700

    service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
-
+import {CartService} from "../cart.service";
 
 @Component({
     selector: 'app-product-details',
@@ -22,9 +22,14 @@ export class ProductDetailsComponent implements OnInit {
 
     constructor(
         private route: ActivatedRoute,
+        private cartService : CartService
     ) { }
 
 
+    addToCart(product) {
+        window.alert('Your Product has been added to the cart!');
+        this.cartService.addToCard(product);
+    }
 
 
 }
