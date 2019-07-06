76fe46c4bed2ec14b7556c7d43517171b0406a43
codegymdanang <codegymdanang@gmail.com>
2019-07-06 14:02:22 +0700

    HTTP Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {CartComponent} from "./cart/cart.component";
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent},
       { path: 'products/:productId', component: ProductDetailsComponent },
