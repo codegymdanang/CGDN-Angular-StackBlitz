bdfe254630996c25dda989281ed03ea2c1d3938d
codegymdanang <codegymdanang@gmail.com>
2019-07-08 16:25:05 +0700

    deployment

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from "@angular/core";
 import { CartService } from "../cart.service";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
     selector : 'app-cart',
@@ -10,11 +11,26 @@ import { CartService } from "../cart.service";
 export class CartComponent {
 
     items;
+    checkoutFrom;
 
     constructor(
-        private cartService: CartService
+        private cartService: CartService,
+        private formBuilder: FormBuilder
     ) {
         this.items = this.cartService.getItems();
+        this.checkoutFrom = this.formBuilder.group({
+            name : '',
+            address : ''
+        });
+
+    }
+
+    onSubmit(customerData) {
+
+        console.warn('your order has been submitted', customerData);
+        this.items = this.cartService.clearCart();
+        this.checkoutFrom.reset();
+
     }
 
 
